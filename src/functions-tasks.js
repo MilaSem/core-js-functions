242797ae41a7728c950433ca90401e5a707ab3a8
MilaSem <milafoxsem@gmail.com>
2025-04-14 13:26:00 +0300

    feat: implement the closure

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -206,8 +206,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+
+  return function fn() {
+    const resultId = currentId;
+    currentId += 1;
+    return resultId;
+  };
 }
 
 module.exports = {
